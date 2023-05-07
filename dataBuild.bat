d43bbfefbad50dfeb04daaad7476ff28cd41366a
billwillman <billwillman@qq.com>
2023-05-07 21:43:40 +0800

    1213123

--- dataBuild.bat
+++ dataBuild.bat
@@ -1,12 +1,15 @@
 cd %~dp0
 
 echo "build flatbuffer data"
-flatc.exe -b ./StreamLoad/Assets/Resources/MonsterCfg.fbs ./StreamLoad/Assets/Resources/MonsterCfg.json
+flatc -b ./StreamLoad/Assets/Resources/MonsterCfg.fbs ./StreamLoad/Assets/Resources/MonsterCfg.json
 echo f | xcopy "%~dp0MonsterCfg.bin" "%~dp0StreamLoad/Assets/Resources/MonsterCfg_flatbuffer.bytes" /y
 del /f %~dp0MonsterCfg.bin
 
 echo "build idMap proto"
 cd %~dp0StreamLoad/Assets/Resources/
 %~dp0protoc --csharp_out=./ IdMap.proto
+
+echo "build net cs"
+%~dp0flatc -n MonsterCfg.fbs
 
 pause
